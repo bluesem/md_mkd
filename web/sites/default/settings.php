e264796c12aead5c435db50d6e6f79e617e05d9d
Marcin Dębicki <info@bluesem.pl>
2025-10-07 11:05:22 +0200

    Updated cofig sync directory.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -866,5 +866,5 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 
-$settings['config_sync_directory'] = '/../config/sync';
+$settings['config_sync_directory'] = '../config/sync';
 
